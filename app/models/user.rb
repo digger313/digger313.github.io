4f2e8737b73bf7d3652e98b442a9a013e98731d1
tatsuya horiuchi <digingdiging@gmail.com>
2020-08-05 00:39:11 +0900

    modify2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  has_secure_password
-
   validates :name, {presence: true, length: {maximum: 20}}
   validates :email, {presence: true, uniqueness: true}
+  has_secure_password
 end
